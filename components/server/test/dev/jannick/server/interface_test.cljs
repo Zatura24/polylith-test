78f731cf4a186ae15733a00492b46201b70dffde
Zatura24 <jannick.joosten@gmail.com>
2025-10-21 22:16:51 +0200

    Add Server component

---
+++ components/server/test/dev/jannick/server/interface_test.cljs
@@ -0,0 +1,6 @@
+(ns dev.jannick.server.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [dev.jannick.server.interface :as server]))
+
+(deftest dummy-test
+  (is (= 1 1)))
